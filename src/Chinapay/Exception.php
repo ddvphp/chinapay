7a5e87a99d77ce99fe9ff36d63368051c932a57e
hua <yuchonghua@163.com>
2018-05-28 22:27:47 +0800

    exc

--- src/Chinapay/Exception.php
+++ src/Chinapay/Exception.php
@@ -4,7 +4,7 @@ namespace DdvPhp\ChinaPay;
 class Exception extends \DdvPhp\DdvException\Error
 {
   // 魔术方法
-  public function __construct( $message = 'Chinapay Error', $code = '400', $errorId = 'CHINA_PAY_ERROR' , $errorData = array() )
+  public function __construct( $message = 'Chinapay Error', $errorId = 'CHINA_PAY_ERROR' , $code = '400', $errorData = array() )
   {
     parent::__construct( $message , $errorId , $code, $errorData );
   }
